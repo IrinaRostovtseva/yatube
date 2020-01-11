31067e04ae9c87311efa0a5fd9a6a4509592c4e1
IrinaRostovtseva <ni8003@yandex.ru>
2020-01-11 14:54:57 +0300

    Create superuser; add and register model Post in administrator interface

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("pk", "text", "publication_date", "author")
+    search_fields = ("text",)
+    list_filter = ("publication_date",)
+    empty_value_display = "-empty-"
+
+admin.site.register(Post, PostAdmin)    
