3f935f450667d122d3db31ab6a794f6aa667a3a9
IrinaRostovtseva <ni8003@yandex.ru>
2020-01-13 21:41:18 +0300

    Add mock DB of posts, urlpatterns for index page and function index in posts/views.py

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -3,9 +3,9 @@ from .models import Post
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("pk", "text", "publication_date", "author")
+    list_display = ("pk", "text", "pub_date", "author")
     search_fields = ("text",)
-    list_filter = ("publication_date",)
+    list_filter = ("pub_date",)
     empty_value_display = "-empty-"
 
 admin.site.register(Post, PostAdmin)    
--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -6,6 +6,6 @@ User = get_user_model()
 
 class Post(models.Model):
     text = models.CharField(max_length=200)
-    publication_date = models.DateTimeField("date published", auto_now_add=True)
+    pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 # Create your models here.
