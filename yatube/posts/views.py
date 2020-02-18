e6d320c9d4aa59fcfa3bfe0d5a54b34bb299b0ea
IrinaRostovtseva <ni8003@yandex.ru>
2020-02-18 22:39:09 +0300

    Add pages for user profile, post edit, post and flatpages for about-author and about-spec

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
-from .models import Post, Group
+from .models import Post, Group, User
 from .forms import PostForm
 
 
@@ -39,3 +39,46 @@ def new_post(request):
         form = PostForm()
         return render(request, "new_post.html", {"form": form})
     return redirect("login")
+
+
+def profile(request, username):
+    user_profile = get_object_or_404(User, username=username)
+    posts = Post.objects.filter(author=user_profile.id).order_by("-pub_date")
+    paginator = Paginator(posts, 5)
+    page_number = request.GET.get("page")
+    page = paginator.get_page(page_number)
+    context = {
+        "user_profile": user_profile,
+        "page": page,
+        "paginator": paginator,
+    }
+    return render(request, "profile.html", context)
+
+
+def post_view(request, username, post_id):
+    user_profile = get_object_or_404(User, username=username)
+    posts_count = Post.objects.filter(author=user_profile.id).count()
+    post = get_object_or_404(Post, id=post_id)
+    context = {
+        "user_profile": user_profile,
+        "post": post,
+        "count": posts_count,
+    }
+    return render(request, "post.html", context)
+
+
+def post_edit(request, username, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if post.author == request.user:
+        if request.method == "POST":
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                form.save(commit=False)
+                form.author = request.user
+                form.save()
+                return redirect("index")
+            return render(request, "new_post.html", {"form": form})
+
+        form = PostForm(instance=post)
+        return render(request, "new_post.html", {"form": form})
+    return redirect("index")
